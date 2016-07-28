e96c08ade68f09595e754af23b898b07fe8993a5
harhogefoo <harimanmon@gmail.com>
2016-07-28 19:00:16 +0900

    init commit

---
+++ Assets/MainMenuBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using DG.Tweening;
+
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnClickedQuitButton()
+	{
+		// Application.Quit;
+	}
+
+	public void OnClickedStartButton()
+	{
+		DOTween.Init (false, true, LogBehaviour.ErrorsOnly);
+		RectTransform rectTrans = GetComponent<RectTransform> ();
+		rectTrans.DOMove (new Vector3 (-1000, 0, 0), 1.0f).OnComplete (() => {
+			SceneManager.LoadScene ("UnityChanScene");
+		});
+	}
+}
---
+++ Assets/PlayerCharacterController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacterController : MonoBehaviour {
+
+	// parameter
+	private float maxSpeed = 10.0f;
+
+	private Animator animator;
+	private Rigidbody2D rb2D;
+	private Transform myTransform;
+
+	void Awake() 
+	{
+		animator = GetComponent<Animator> ();
+		rb2D = GetComponent<Rigidbody2D> ();
+		myTransform = transform;
+	}
+
+	void Update () 
+	{
+		float move = Input.GetAxis ("Horizontal");
+		if (Mathf.Abs (move) > 0) {
+			// 進む方向に寄って向きを変える
+			Quaternion rot = myTransform.rotation;
+			float newRotY = Mathf.Sign (move) == 1 ? 0 : 180;
+			myTransform.rotation = Quaternion.Euler (rot.x, newRotY, rot.z);
+			// AnimatorのHorizontalパラメータに入力値(move)を渡す
+			animator.SetFloat("Horizontal", move);
+			// 移動処理
+			rb2D.velocity = new Vector2(move * maxSpeed, rb2D.velocity.y);
+		}
+	}
+}
